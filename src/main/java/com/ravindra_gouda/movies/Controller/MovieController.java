2f17e99a70da3012ebe629dbaa42c66da578680d
ravindra_gouda <ravindragouda444@gmail.com>
2024-08-20 21:28:45 +0530

    added movie and review model

---
+++ src/main/java/com/ravindra_gouda/movies/Controller/MovieController.java
@@ -0,0 +1,16 @@
+package com.ravindra_gouda.movies.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @GetMapping()
+    public  String allMovies(){
+        return "All movies";
+    }
+
+}
---
+++ src/test/java/com/ravindra_gouda/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravindra_gouda.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
